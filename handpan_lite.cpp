d2c7b93b3b933144f9e614decb10fdc38e4bebc8
FabianMartinez83 <fabian.martinez3121983@gmail.com>
2025-06-09 20:58:27 +0200

    adjusted int bus

--- handpan_lite.cpp
+++ handpan_lite.cpp
@@ -85,7 +85,7 @@ static const _NT_parameterPages parameterPages = {
 
 
 inline float* _NT_getAudioInput(_NT_algorithm* self, int index, float* busFrames, int numFrames) {
-    int bus = self->v[index];
+    int bus = self->v[index] - 1;  // Adjust for 0-based indexing
     return busFrames + bus * numFrames;
 }
 
